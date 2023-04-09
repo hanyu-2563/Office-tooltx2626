34e669e75c1965cb006f0e58e825d2148a7098d5
Taedigrade <61576316+hanyu-2563@users.noreply.github.com>
2023-04-09 23:51:40 +0800

    0409
    add-01

---
+++ e5/using Microsoft.Office.Interop0409.cs
@@ -0,0 +1,27 @@
+using Microsoft.Office.Interop.Word;
+
+public void ConvertNumberedListToText()
+{
+    // 获取当前Word应用程序实例
+    Application wordApp = Globals.ThisAddIn.Application;
+
+    // 获取所选段落
+    Paragraph selectedPara = wordApp.Selection.Range.Paragraphs[1];
+
+    // 检查是否为数字列表
+    if (selectedPara.Range.ListFormat.ListType == WdListType.wdListBullet
+        || selectedPara.Range.ListFormat.ListType == WdListType.wdListNoNumbered
+        || selectedPara.Range.ListFormat.ListType == WdListType.wdListOutlineNumbering)
+    {
+        // 将列表项的格式设置为文本格式
+        selectedPara.Range.ListFormat.ApplyListTemplate(
+            ListTemplate: wordApp.ListGalleries[WdListGalleryType.wdNumberGallery].ListTemplates[3],
+            ContinuePreviousList: false,
+            ApplyTo: WdListApplyTo.wdListApplyToWholeList,
+            DefaultListBehavior: WdDefaultListBehavior.wdWord10ListBehavior);
+
+        // 更新段落样式，确保正确呈现文本格式的列表项
+        selectedPara.Range.Select();
+        wordApp.Selection.ParagraphFormat.Update();
+    }
+}
